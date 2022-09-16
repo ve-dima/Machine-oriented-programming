0cc9f48f55748c06220b7a764dc94ed2af2eff0d
ve-dima <y200323@yandex.ru>
2022-09-16 13:16:12 +0300

    add

--- LB1.asm
+++ LB1.asm
@@ -1,9 +1,9 @@
 model SMALL     ; Модель памяти: small
 stack 100h      ; Размер стэка: 256 байт
 dataseg         ; Определения начало инициализированного сегмента данных
-A dw 15          ; Определение переменной A размером 2 байта
+A dw 15         ; Определение переменной A размером 2 байта
 B dw 88         ; Определение переменной B размером 2 байта
-C dw 32          ; Определение переменной C размером 2 байта
+C dw 32         ; Определение переменной C размером 2 байта
 X dw ?          ; Определение переменной X размером 2 байта
 codeseg         ; Определение начала кода
 startupcode     ; Обеспечивает код инициализации и отмечает начало программы
@@ -11,21 +11,17 @@ startupcode     ; Обеспечивает код инициализации и отмечает начало программы
 mov AX, A       ; AX = A
 mov BX, B       ; BX = B
 
-sub AX, BX      ; AX = AX - BX
+sub AX, BX      ; AX = (A - B)
 
-cwd             ; DX:AX расширенный AX
-mov BX, 4       ; BX = 4
-idiv BX         ; AX = DX:AX / BX, DX = DX:AX mod BX
+sar AX, 2       ; AX = (A - B) / 4
 
-mov BX, AX      ; BX = AX
-mov AX, C       ; AX = C
-mov DX, 2       ; DX = 2
-imul DX         ; DX:AX = AX * DX
+mov BX, C       ; BX = C
+sal BX, 1       ; BX = C * 2
 
-SUB BX, AX      ; BX = BX - AX
-ADD BX, 5       ; BX = BX + 5
+sub AX, BX      ; AX = (A - B) / 4 - 2C
+add AX, 5       ; AX = (A - B) / 4 - 2C + 5
 
-mov X, BX       ; X = BX
+mov X, AX       ; X = AX = (A - B) / 4 - 2C + 5
 
 exitcode 0      ; Генерирует код завершения
 end
